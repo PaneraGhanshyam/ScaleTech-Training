018f848cbb1c5423bd64db65229e8c2476709de0
PaneraGhanshyam <110153943+PaneraGhanshyam@users.noreply.github.com>
2026-09-15 19:29:19 +0530

    Learn: worked on Django URL Routing, Static Files, and Test Cases

--- stage-04/django_basics/django_basics/urls.py
+++ stage-04/django_basics/django_basics/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from users.views import UserListView
+from django.urls import path,include
+# from users.views import UserListView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", UserListView.as_view()),
+    path("users/", include("users.urls")),
+    # path("", UserListView.as_view()),
 ]
--- stage-04/django_basics/users/tests.py
+++ stage-04/django_basics/users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import UserProfile
 
-# Create your tests here.
+
+class UserProfileTestCase(TestCase):
+
+    def test_user_creation(self):
+        user = UserProfile.objects.create(
+            name="Ghanshyam",
+            email="ghanshyam@example.com",
+            age=22
+        )
+
+        self.assertEqual(user.name, "Ghanshyam")
+        self.assertEqual(user.age, 22)
